1d416b9944864dcbfe70a7d95559299be36f55ca
4face-studi0 <4face.studio@gmail.com>
2020-12-16 19:18:12 +0100

    Setup Gradle publish plugin

---
+++ gradle/publishPlugin/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    `kotlin-dsl`
+    kotlin("jvm") version "1.4.21"
+    id("java-gradle-plugin")
+}
+
+group = "studio.forface.cinescout"
+version = "1.0"
+
+gradlePlugin {
+    plugins {
+        create("publishPlugin") {
+            id = "publish"
+            implementationClass = "PublishPlugin"
+        }
+    }
+}
+
+repositories {
+    google()
+    jcenter()
+}
+
+dependencies {
+    val easyGradle = "2.8" // Nov 25, 2020
+
+    implementation(gradleApi())
+    implementation("studio.forface.easygradle:dsl:$easyGradle")
+}
---
+++ gradle/publishPlugin/src/main/kotlin/PublishPlugin.kt
@@ -0,0 +1,18 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.register
+import java.io.File
+
+abstract class PublishPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        target.run {
+
+            tasks.register<BumpVersionIfNeededTask>("bumpVersionIfNeeded") {
+                val file = File(project.projectDir, "releases.txt")
+                if (file.exists().not()) file.createNewFile()
+                versionFile.set(File("releases.txt"))
+            }
+
+        }
+    }
+}
